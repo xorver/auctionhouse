4e3d96030ab34b7e400e19858ea183c7cc88ad4c
Tomasz Lichon <lichon@student.agh.edu.pl>
2014-10-23 12:15:04 +0200

    seller + buyer changes

--- src/main/scala/auctionhouse/actors/Buyer.scala
+++ src/main/scala/auctionhouse/actors/Buyer.scala
@@ -17,16 +17,17 @@ final case class WakeUp extends BuyerMessage
 final case class StartWorking extends BuyerMessage
 
 
-class Buyer(auctionsToBid: List[ActorRef], auctionsToStart: List[ActorRef])  extends Actor {
+class Buyer(auctionNames: List[String])  extends Actor {
   val log = Logging(context.system, this)
   val rand = new Random(System.currentTimeMillis());
-  auctionsToStart.foreach(ref => ref ! Start(5000 + rand.nextInt(10000)))
   
   def receive = {
     case WakeUp =>
 //	  log.info(s"$self has woke up !")	
-      val auction = auctionsToBid(rand.nextInt(auctionsToBid.length))
-      auction ! Bid(rand.nextInt(1000))
+      val auctionName = auctionNames(rand.nextInt(auctionNames.length))
+//      val future = aaa ? GFFF()
+//      future.wait()
+//      auction ! Bid(rand.nextInt(1000))
       context.system.scheduler.scheduleOnce(Duration.create(rand.nextInt(1000), TimeUnit.MILLISECONDS), self, WakeUp)
     case AuctionWon =>
       log.info(s"Buyer won an auction ${sender.path.name} !")
